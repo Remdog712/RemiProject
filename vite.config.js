ddd6da24cbc192aa8be521b048f877eef9e04af6
Remington Orange <remingtonorange@yahoo.com>
2024-11-05 10:03:45 -0500

    add

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'dist', // Set 'dist' as the root directory
-  publicDir: '../public', // Keep publicDir outside 'dist'
+  root: 'dist', 
+  publicDir: '../public', 
   build: {
-    outDir: 'dist', // Ensure build output goes to 'dist'
+    outDir: 'dist', 
   },
 });
